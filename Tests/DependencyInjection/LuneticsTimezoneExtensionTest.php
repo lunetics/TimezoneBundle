a173048e2985ad6c6ca1ece84c42be25ba869ff0
Matthias Breddin <mb@lunetics.com>
2012-10-27 17:13:47 +0200

    Use Event Subscriber Add listener for timezone update Fixed Docs

--- Tests/DependencyInjection/LuneticsTimezoneExtensionTest.php
+++ Tests/DependencyInjection/LuneticsTimezoneExtensionTest.php
@@ -1,11 +1,11 @@
 <?php
-
-/*
- * This file is part of the LuneticsLocaleBundle package.
+/**
+ * This file is part of the LuneticsTimezoneBundle package.
  *
+ * <https://github.com/lunetics/TimezoneBundle/>
  *
  * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * file that is distributed with this source code.
  */
 
 namespace Lunetics\TimezoneBundle\Tests\DependencyInjection;
